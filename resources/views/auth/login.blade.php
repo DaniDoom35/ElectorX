cd454d439593623afd76bd0f0f634cb804af4a78
DaniDoom35 <125550224+DaniDoom35@users.noreply.github.com>
2024-03-07 14:10:47 -0600

    Update del Login
    ...

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,68 +16,128 @@
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
-
-        <link rel="stylesheet" href="{{asset('assets/styles.css')}}">
-
+        <link rel="stylesheet" href="{{ asset('assets/styles.css') }}">
     </head>
 
     <body>
-        <section class="vh-100">
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-sm-6 text-black">
+        <section class="background-radial-gradient overflow-hidden">
+            <style>
+              .background-radial-gradient {
+                background-color: hsl(218, 41%, 15%);
+                background-image: radial-gradient(650px circle at 0% 0%,
+                    hsl(218, 41%, 35%) 15%,
+                    hsl(218, 41%, 30%) 35%,
+                    hsl(218, 41%, 20%) 75%,
+                    hsl(218, 41%, 19%) 80%,
+                    transparent 100%),
+                  radial-gradient(1250px circle at 100% 100%,
+                    hsl(218, 41%, 45%) 15%,
+                    hsl(218, 41%, 30%) 35%,
+                    hsl(218, 41%, 20%) 75%,
+                    hsl(218, 41%, 19%) 80%,
+                    transparent 100%);
+              }
 
-                  <div class="px-5 ms-xl-4">
-                    <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-                    <span class="h1 fw-bold mb-0">Logo</span>
-                  </div>
+              #radius-shape-1 {
+                height: 220px;
+                width: 220px;
+                top: -60px;
+                left: -130px;
+                background: radial-gradient(#44006b, #ad1fff);
+                overflow: hidden;
+              }
 
-                  <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+              #radius-shape-2 {
+                border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
+                bottom: -60px;
+                right: -110px;
+                width: 300px;
+                height: 300px;
+                background: radial-gradient(#44006b, #ad1fff);
+                overflow: hidden;
+              }
 
-                    <form style="width: 23rem;">
+              .bg-glass {
+                background-color: hsla(0, 0%, 100%, 0.9) !important;
+                backdrop-filter: saturate(200%) blur(25px);
+              }
+            </style>
 
-                      <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
-
-                      <div class="form-outline mb-4">
-                        <input type="email" id="form2Example18" class="form-control form-control-lg" />
-                        <label class="form-label" for="form2Example18">Email address</label>
-                      </div>
-
-                      <div class="form-outline mb-4">
-                        <input type="password" id="form2Example28" class="form-control form-control-lg" />
-                        <label class="form-label" for="form2Example28">Password</label>
-                      </div>
-
-                      <div class="pt-1 mb-4">
-                        <button class="btn btn-info btn-lg btn-block" type="button">Login</button>
-                      </div>
-
-                      <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
-                      <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
-
-                    </form>
-
-                  </div>
-
+            <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+              <div class="row gx-lg-5 align-items-center mb-5">
+                <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+                  <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+                    The best offer <br />
+                    <span style="color: hsl(218, 81%, 75%)">for your business</span>
+                  </h1>
+                  <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Temporibus, expedita iusto veniam atque, magni tempora mollitia
+                    dolorum consequatur nulla, neque debitis eos reprehenderit quasi
+                    ab ipsum nisi dolorem modi. Quos?
+                  </p>
                 </div>
-                <div class="col-sm-6 px-0 d-none d-sm-block">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
-                    alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+
+                <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+                  <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
+                  <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+
+                  <div class="card bg-glass">
+                    <div class="card-body px-4 py-5 px-md-5">
+
+                      <form action="{{route('login')}}" method="post">
+                        @csrf
+                        <div class="mb-4">
+                          <h2 class="text-center">Login</h2>
+
+                          <p class="text-center">Ingresa tus datos</p>
+
+                        <!-- CURP input -->
+                        <div class="mb-4">
+                          <div class="form-outline">
+                            <input type="text" name="curp" id="curp" class="form-control"
+                            placeholder="Ingresa tu CURP"/>
+                            <label class="form-label" for="curp">CURP</label>
+                          </div>
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="form-outline mb-4">
+                          <input type="password" name="password" id="password" class="form-control"
+                          placeholder="Ingresa tu contraseña" />
+                          <label class="form-label" for="password">Contraseña</label>
+                        </div>
+
+                        <!-- Entrar button -->
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary btn-block mb-4">
+                              Entrar
+                            </button>
+                          </div>
+
+                        <!-- Olvidar Password -->
+
+                       <!-- Forgot password link -->
+                <div class="d-flex justify-content-center mb-3">
+                    <a href="{{ route('password.request') }}" class="text-primary">Forgot password?</a>
+                </div>
+
+                <!-- Registro button -->
+                <div class="d-flex align-items-center justify-content-center pb-4">
+                    <p class="mb-0 me-2"> No estas registrado?</p>
+                    <a href="{{route('register')}}" class="btn btn-secondary btn-block">
+                        Registrarse
+                    </a>
+                </div>
+
+                            </a>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </section>
-        <!-- Bootstrap JavaScript Libraries -->
-        <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"
-        ></script>
-
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
     </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,143 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="{{ asset('assets/styles.css') }}">
+    </head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <body>
+        <section class="background-radial-gradient overflow-hidden">
+            <style>
+              .background-radial-gradient {
+                background-color: hsl(218, 41%, 15%);
+                background-image: radial-gradient(650px circle at 0% 0%,
+                    hsl(218, 41%, 35%) 15%,
+                    hsl(218, 41%, 30%) 35%,
+                    hsl(218, 41%, 20%) 75%,
+                    hsl(218, 41%, 19%) 80%,
+                    transparent 100%),
+                  radial-gradient(1250px circle at 100% 100%,
+                    hsl(218, 41%, 45%) 15%,
+                    hsl(218, 41%, 30%) 35%,
+                    hsl(218, 41%, 20%) 75%,
+                    hsl(218, 41%, 19%) 80%,
+                    transparent 100%);
+              }
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+              #radius-shape-1 {
+                height: 220px;
+                width: 220px;
+                top: -60px;
+                left: -130px;
+                background: radial-gradient(#44006b, #ad1fff);
+                overflow: hidden;
+              }
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+              #radius-shape-2 {
+                border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
+                bottom: -60px;
+                right: -110px;
+                width: 300px;
+                height: 300px;
+                background: radial-gradient(#44006b, #ad1fff);
+                overflow: hidden;
+              }
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+              .bg-glass {
+                background-color: hsla(0, 0%, 100%, 0.9) !important;
+                backdrop-filter: saturate(200%) blur(25px);
+              }
+            </style>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+              <div class="row gx-lg-5 align-items-center mb-5">
+                <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+                  <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+                    The best offer <br />
+                    <span style="color: hsl(218, 81%, 75%)">for your business</span>
+                  </h1>
+                  <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Temporibus, expedita iusto veniam atque, magni tempora mollitia
+                    dolorum consequatur nulla, neque debitis eos reprehenderit quasi
+                    ab ipsum nisi dolorem modi. Quos?
+                  </p>
                 </div>
+
+                <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+                  <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
+                  <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+
+                  <div class="card bg-glass">
+                    <div class="card-body px-4 py-5 px-md-5">
+
+                      <form action="{{route('login')}}" method="post">
+                        @csrf
+                        <div class="mb-4">
+                          <h2 class="text-center">Login</h2>
+
+                          <p class="text-center">Ingresa tus datos</p>
+
+                        <!-- CURP input -->
+                        <div class="mb-4">
+                          <div class="form-outline">
+                            <input type="text" name="curp" id="curp" class="form-control"
+                            placeholder="Ingresa tu CURP"/>
+                            <label class="form-label" for="curp">CURP</label>
+                          </div>
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="form-outline mb-4">
+                          <input type="password" name="password" id="password" class="form-control"
+                          placeholder="Ingresa tu contraseña" />
+                          <label class="form-label" for="password">Contraseña</label>
+                        </div>
+
+                        <!-- Entrar button -->
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary btn-block mb-4">
+                              Entrar
+                            </button>
+                          </div>
+
+                        <!-- Olvidar Password -->
+
+                       <!-- Forgot password link -->
+                <div class="d-flex justify-content-center mb-3">
+                    <a href="{{ route('password.request') }}" class="text-primary">Forgot password?</a>
+                </div>
+
+                <!-- Registro button -->
+                <div class="d-flex align-items-center justify-content-center pb-4">
+                    <p class="mb-0 me-2"> No estas registrado? </p>
+                    <a href="" class="btn btn-secondary btn-block">
+                        Registrarse
+                    </a>
+                </div>
+
+                            </a>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+          </section>
+    </body>
+</html>
